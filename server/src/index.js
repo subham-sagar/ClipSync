4def371b0fb4ab20db011ac6845a5b13196c3c04
subham-sagar <subhamsagarp934@gmail.com>
2025-11-24 19:33:32 +0530

    Fix build script for Vercel deployment

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,11 @@ import '@dotenvx/dotenvx/config';
 import connectDB from './dbConnect/dbConnection.js';
 import { app } from './app.js';
 
+app.get("/", (req, res) => {
+  res.send("Backend working!");
+});
+
+
 connectDB().then(() => {
     app.listen(process.env.PORT || 8000 , () =>{
         console.log(`Server is running at port ${process.env.PORT}`);
